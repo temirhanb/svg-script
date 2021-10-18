5899e73dc001855f8d7c3b15e4c7477ff38daa10
temirhanb <temirhan023@gmaul.com>
2021-10-18 19:58:07 +0500

    - new pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-const imageCreate = (text = 'Иван Иванович') => {
+const imageTextCreate = (text = 'Иван Иванович') => {
   const svgText = document.getElementById("svg_text");
   svgText.innerHTML = text;
 }
 
-imageCreate()
+const authorization = (props) => {
+  props.stopPropagation()
+}
+
+const openCertificate = (props) => {
+  props.stopPropagation()
+  const text = document.getElementById("fio_input");
+  imageTextCreate(text)
+}
