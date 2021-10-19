1ea180623b85d80521fd7abd99a9171fcb20bfcf
temirhanb <temirhan023@gmaul.com>
2021-10-19 20:12:42 +0500

    build 1.01

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3006;
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
+  // res.sendFile(path.join(__dirname + '/form.html'));
 });
+app.use("/src", express.static('./src'));
+app.use("/", express.static('./'));
 
 app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
