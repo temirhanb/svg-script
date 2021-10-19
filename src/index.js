fa035c3bc7e11b76df68349264cfcab66e605e65
temirhanb <temirhan023@gmaul.com>
2021-10-19 17:24:35 +0500

    - end fix

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
-const imageTextCreate = (text = 'Иван Иванович') => {
-  const svgText = document.getElementById("svg_text");
-  svgText.innerHTML = text;
-}
 
-const authorization = (props) => {
-  props.stopPropagation()
-}
 
-const openCertificate = (props) => {
+const authorization = async (props) => {
   props.stopPropagation()
-  const text = document.getElementById("fio_input");
-  imageTextCreate(text)
+  const login = document.getElementById('login_id').value;
+  const password = document.getElementById('password_id').value;
+  console.log(login, password)
+  const url = 'https://sirius-crm-api.herokuapp.com/api/v1/certificate_login'
+  try {
+    await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *client
+      body: JSON.stringify({
+        login, password
+      }) // body data type must match "Content-Type" header
+    }).then((res) => {
+      if (res.ok) document.location.href = "form.html";
+      else {
+        const error = document.getElementById('error')
+        error.innerHTML = 'Что-то пошло не так'
+      }
+    })
+  } catch (err) {
+    console.log(err)
+  }
 }
